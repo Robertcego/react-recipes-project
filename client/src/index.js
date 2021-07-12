b55025e7d39c64c78b2d87806c5c2989140055c1
Robertcego <lrobertito@gmail.com>
2021-07-12 16:25:28 -0600

    Update

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store/index';
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
---
+++ client/src/reducers/index.js
@@ -0,0 +1,54 @@
+import {
+  GET_RECIPES,
+  GET_RECIPE_DETAILS,
+  GET_RECIPE_NAMES,
+  GET_DIETS,
+  POST_RECIPE,
+} from '../actions/index';
+
+const initialState = {
+  getRecipes: [],
+  recipeDetails: '',
+  getRecipesName: [],
+  getDiets: [],
+  postRecipe: {},
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_RECIPES:
+      return {
+        ...state,
+        getRecipes: action.payload,
+      };
+    case GET_RECIPE_DETAILS: {
+      return {
+        ...state,
+        recipeDetails: action.payload,
+      };
+    }
+    case GET_RECIPE_NAMES: {
+      return {
+        ...state,
+        getRecipesName: action.payload,
+      };
+    }
+    case GET_DIETS: {
+      return {
+        ...state,
+        getDiets: action.payload,
+      };
+    }
+    case POST_RECIPE: {
+      return {
+        ...state,
+        postRecipe: action.payload,
+      };
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
